21e9410969c791a205764f37247b90daa10903c3
Matthis CUSIN <matthis.cusin@epitech.eu>
2020-02-23 12:16:12 +0100

    add gravity scripts

---
+++ Assets/Scripts/Gravity/FauxGravityBody.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour
+{
+    public FauxGravityAttractor attractor;
+    private Transform myTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rigidbody = gameObject.GetComponent<Rigidbody>();
+        rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        rigidbody.useGravity = false;
+        myTransform = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attractor != null)
+            attractor.Attract(myTransform);
+    }
+}
